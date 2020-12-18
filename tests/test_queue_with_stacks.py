3949848faf6581b7b4421fbbcfe977a69042730c
sondos-braim <sondos9811@gmail.com>
2020-12-18 11:25:01 +0200

    queue-with-stacks-tests

--- tests/test_queue_with_stacks.py
+++ tests/test_queue_with_stacks.py
@@ -7,3 +7,15 @@ def test_queue_with_stacks():
     q.enqueue(1)
     q.enqueue(2)
     assert q.dequeue()==1
+    q.enqueue(5)
+    q.enqueue(9)
+    q.enqueue(3)
+    assert q.dequeue()==2
+    assert q.dequeue()==5
+    assert q.stack1.peek()=='Stack is empty'
+    assert q.stack2.peek()==9
+    q.enqueue(8)
+    assert q.stack1.peek()==8
+    assert q.stack2.peek()=='Stack is empty'
+
+
